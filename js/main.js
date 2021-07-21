bb746b58a30f320c4287f3e35ba8a3371360d0bf
Mirko <mirkoanna137@gmail.com>
2021-07-21 10:42:47 +0200

    Correzione esercizio

--- js/main.js
+++ js/main.js
@@ -1,19 +1,16 @@
-/* Chiedi all’utente il cognome
-Inseriscilo in un array con altri cognomi: 'Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’*/
+// Chiedi all’utente il cognome
+var nuovoCognome = prompt('Inserisci il tuo cognome');
 
+// Inseriscilo in un array con altri cognomi: 'Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
 var cognomi = ['Bianchi', 'Neri', 'Rossi', 'Verdi', 'Gialli'];
+console.log(cognomi);
 
-var i = 0;
+var CognomeMaiuscolo = nuovoCognome.charAt(0).toUpperCase() + nuovoCognome.slice(1);
 
-while (i < 1) {
-    var nuovoCognome = prompt('Inserisci il tuo cognome');
-    i++
+cognomi.push(CognomeMaiuscolo);
 
-    cognomi.push(nuovoCognome);
-}
-
-cognomi.sort();
 // Stampa la lista ordinata alfabeticamente
+cognomi.sort();
 console.log(cognomi)
 // Scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
-console.log(cognomi.indexOf(nuovoCognome));
+console.log(cognomi.indexOf(CognomeMaiuscolo) + 1);
